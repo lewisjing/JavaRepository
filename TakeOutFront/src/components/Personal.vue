b4ce8552be658f9c1d00c58cf7921340f6d63c06
lewis.jing <lewisjing@augmentum.com.cn>
2019-03-06 10:00:22 +0800

    finish personal page

--- TakeOutFront/src/components/Personal.vue
+++ TakeOutFront/src/components/Personal.vue
@@ -34,10 +34,48 @@
             <span class="person-integral-item">
                 <span>
                     <span class="person-integral-score person-grade">960</span>
-                    积分
+                    分
                 </span>
                 <span class="person-integral-name">我的积分</span>
             </span>
+        </div>
+
+        <div class="person-options">
+            <div class="person-option-item">
+                <span class="person-option-icon">
+                    <img src="../../static/images/person_order.png" />
+                    <span class="person-option-name">我的积分</span>
+                </span>
+                <img src="../../static/images/right_arrow.png" />
+            </div>
+            <div class="person-option-item">
+                <span class="person-option-icon">
+                    <img src="../../static/images/person_shopping.png" />
+                    <span class="person-option-name">积分商城</span>
+                </span>
+                <img src="../../static/images/right_arrow.png" />
+            </div>
+            <div class="person-option-item">
+                <span class="person-option-icon">
+                    <img src="../../static/images/crown.png" />
+                    <span class="person-option-name">饿了么会员卡</span>
+                </span>
+                <img src="../../static/images/right_arrow.png" />
+            </div>
+            <div class="person-option-item">
+                <span class="person-option-icon">
+                    <img src="../../static/images/service.png" />
+                    <span class="person-option-name">服务中心</span>
+                </span>
+                <img src="../../static/images/right_arrow.png" />
+            </div>
+            <div class="person-option-item">
+                <span class="person-option-icon">
+                    <img src="../../static/images/eleme.png" />
+                    <span class="person-option-name">下载饿了么APP</span>
+                </span>
+                <img src="../../static/images/right_arrow.png" />
+            </div>
         </div>
     </div>
 </template>
@@ -120,6 +158,30 @@ export default {
 
             .person-grade {
                 color: #6ac20b
+            }
+        }
+    }
+
+    .person-options {
+        padding-top: 10px;
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        .person-option-item {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 5px;
+
+            .person-option-icon {
+                display: inline-flex;
+                align-items: center;
+
+                .person-option-name {
+                    margin-left: 5px;
+                }
             }
         }
     }
